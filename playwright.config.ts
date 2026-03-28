5bf2f0b1f0680bce6126709a63c646022a435965
ejsadiarin <ejs@ejsadiarin.com>
2026-03-29 00:05:36 +0800

    chore(setup): add playwright webServer timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,5 +9,6 @@ export default defineConfig({
     command: 'npm run dev -- --host 127.0.0.1 --port 4321',
     url: 'http://127.0.0.1:4321',
     reuseExistingServer: !process.env.CI,
+    timeout: 120000,
   },
 });
